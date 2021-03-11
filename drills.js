efb2041420eab3d5f8736765c64b5f7058ade8b6
a_frear <amy.frear@gmail.com>
2021-03-11 08:19:27 -0500

    initial commit

---
+++ drills.js
@@ -0,0 +1,126 @@
+const BinarySearchTree = require('./BST')
+const Queue = require('./queue')
+
+/*
+1. They each will have 4 searches.
+First: 11, 5, 6, then 8
+Second: 11, 14, 15, 17.... not there!
+*/
+
+/*
+1) Given a binary search tree whose in-order and pre-order traversals are respectively 
+14 15 19 25 27 35 79 89 90 91 and 35 25 15 14 19 27 89 79 91 90. 
+What would be its postorder traversal?
+
+Postorder Answer: 14, 19, 15, 27, 25, 90, 91, 79, 89, 35
+
+The post order traversal of a binary search tree is 5 7 6 9 11 10 8. 
+What is its pre-order traversal?
+Answer: 8, 6, 5, 10, 9, 11
+*/
+
+/*
+Using your BinarySearchTree class from your previous lesson, 
+create a binary search tree with the following dataset: 
+25 15 50 10 24 35 70 4 12 18 31 44 66 90 22. 
+Then implement inOrder(), preOrder(), and postOrder() functions. 
+*/
+const BST = new BinarySearchTree();
+const treeData = [25, 15, 50, 10, 24, 35, 70, 4, 12, 18, 31, 44, 66, 90, 22];
+
+function fillTree(arr, bst) {
+  let tree = bst;
+  for (let i = 0; i < arr.length; i++) {
+    tree.insert(arr[i], arr[i]);
+  }
+  return tree;
+}
+
+fillTree(treeData, BST);
+
+function preOrder(bst) {
+  let node = bst;
+  //node handled first
+  console.log(node.key);
+  //then left
+  if (node.left) {
+    preOrder(node.left);
+  }
+  //then right
+  if (node.right) {
+    preOrder(node.right);
+  }
+}
+console.log("Pre-order");
+preOrder(BST);
+
+function postOrder(bst) {
+  let node = bst;
+  //left branch first
+  if (node.left) {
+    postOrder(node.left);
+  }
+  //then right branch
+  if (node.right) {
+    postOrder(node.right);
+  }
+  //node handled after the branches
+  console.log(node.key);
+}
+console.log("Post-order");
+postOrder(BST);
+
+function inOrder(bst) {
+  let node = bst;
+  //left branch is visited
+  if (node.left) {
+    inOrder(node.left);
+  }
+  //then node itself
+  console.log(node.key);
+  //then right branch
+  if (node.right) {
+    postOrder(node.right);
+  }
+}
+console.log("In-Order");
+inOrder(BST);
+
+// STAR TREK NEXT COMMANDING OFFICER
+const CommanderTree = new BinarySearchTree();
+
+//use postorder traversal to insert values
+CommanderTree.insert(5, "Captain Picard");
+CommanderTree.insert(3, "Commander Riker");
+CommanderTree.insert(6, "Commander Data");
+CommanderTree.insert(8, "Lt. Cmdr. Crusher");
+CommanderTree.insert(7, "Lieutenant Selar");
+CommanderTree.insert(2, "Lt. Cmdr. Worf");
+CommanderTree.insert(4, "Lt. Cmdr. LaForge");
+CommanderTree.insert(1, "Lt. Security-Officer");
+
+function nextInCommand(tree) {
+  const result = []  
+  //create a new queue
+  const CommanderQueue = new Queue();
+  //create a queue out of our bst
+  CommanderQueue.enqueue(tree);
+  //while first in the queue is not null
+  while (CommanderQueue.first !== null) {
+    const node = CommanderQueue.dequeue();
+    result.push(node.value);
+    if (node.left) {
+      CommanderQueue.enqueue(node.left);
+    }
+    if (node.right) {
+      CommanderQueue.enqueue(node.right);
+    }
+  }
+  result.forEach((officers) => console.log(officers));
+}
+
+nextInCommand(CommanderTree);
+
+// Max Profit
+
+  
